c92c219d8ef632ceb0f6a91f62f773e10d08a1f7
HenriqueValente09 <valentedev09@gmail.com>
2023-09-29 16:10:05 -0300

    feat: user DTO converter

---
+++ src/main/java/com/valenteNews/newsApp/dto/UserDtoConverter.java
@@ -0,0 +1,27 @@
+package com.valenteNews.newsApp.dto;
+
+import com.valenteNews.newsApp.model.User;
+import com.valenteNews.newsApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDtoConverter{
+
+    @Autowired
+    private UserService userService;
+
+    public UserDTO toDTO(User user) {
+        UserDTO dto = new UserDTO();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        return dto;
+    }
+
+    public User toUser(UserDTO userDTO) {
+        User user = new User();
+        user.setName(userDTO.getName());
+        return user;
+    }
+
+}
