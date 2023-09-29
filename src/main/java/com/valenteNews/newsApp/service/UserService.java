c0b877b5144b8edfbc724e9f373856e7e85bb9f8
HenriqueValente09 <valentedev09@gmail.com>
2023-09-29 08:26:06 -0300

    feat: get all users function

--- src/main/java/com/valenteNews/newsApp/service/UserService.java
+++ src/main/java/com/valenteNews/newsApp/service/UserService.java
@@ -1,21 +1,30 @@
 package com.valenteNews.newsApp.service;
 
+import com.valenteNews.newsApp.dto.UserDTO;
 import com.valenteNews.newsApp.model.User;
 import com.valenteNews.newsApp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
-
     @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    private UserRepository userRepository;
+
+    public User save(User user) {
+        try{
+            return userRepository.save(user);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
     }
 
-    public void saveUser(User user) {
-        userRepository.save(user);
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
