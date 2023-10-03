e1b422c00d8f46c5051df48c75588d3fbf456e0b
HenriqueValente09 <valentedev09@gmail.com>
2023-10-03 10:57:54 -0300

    chore: home page recent posts

--- src/main/java/com/valenteNews/newsApp/controller/MainController.java
+++ src/main/java/com/valenteNews/newsApp/controller/MainController.java
@@ -1,23 +1,53 @@
 package com.valenteNews.newsApp.controller;
 
+import com.valenteNews.newsApp.dto.post.PostDTO;
 import com.valenteNews.newsApp.dto.user.UserDTO;
 import com.valenteNews.newsApp.dto.user.UserDtoConverter;
+import com.valenteNews.newsApp.model.Post;
+import com.valenteNews.newsApp.service.PostService;
 import com.valenteNews.newsApp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 @RequiredArgsConstructor
 public class MainController {
-    private final UserService userService;
+    private final PostService postService;
     private final UserDtoConverter userDtoConverter;
 
     @GetMapping("/")
     public String home(Model model) {
-        UserDTO userDTO = new UserDTO();
-        model.addAttribute("userDTO", userDTO);
+        List<Post> recentPosts = postService.getAllPosts();
+
+        recentPosts = recentPosts.stream()
+                .sorted(Comparator.comparing(Post::getCreatedAt).reversed())
+                .collect(Collectors.toList());
+
+        recentPosts = recentPosts.stream()
+                .limit(3)
+                .collect(Collectors.toList());
+
+        List<PostDTO> recentPostsDTO = new ArrayList<>();
+
+        for (Post post : recentPosts) {
+            PostDTO postDTO = new PostDTO();
+            postDTO.setId(post.getId());
+            postDTO.setTitle(post.getTitle());
+            postDTO.setContent(post.getContent());
+            postDTO.setImageURL(post.getImageURL());
+
+            recentPostsDTO.add(postDTO);
+        }
+
+        model.addAttribute("posts", postService.getAllPosts());
+        model.addAttribute("recentPosts", recentPostsDTO);
         return "home";
     }
 }
