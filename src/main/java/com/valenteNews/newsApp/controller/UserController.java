0254461e97d6eec2f1e643661950b8e8f7dab77e
HenriqueValente09 <valentedev09@gmail.com>
2023-10-03 10:52:59 -0300

    chore: testing post list attribute

--- src/main/java/com/valenteNews/newsApp/controller/UserController.java
+++ src/main/java/com/valenteNews/newsApp/controller/UserController.java
@@ -2,6 +2,7 @@ package com.valenteNews.newsApp.controller;
 
 import com.valenteNews.newsApp.dto.user.UserDTO;
 import com.valenteNews.newsApp.dto.user.UserDtoConverter;
+import com.valenteNews.newsApp.model.Post;
 import com.valenteNews.newsApp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,6 +21,8 @@ public class UserController {
 
     @GetMapping("/users")
     public String users(Model model) {
+        List<Post> userPosts = userService.getUserByName("João").get().getPosts();
+        System.out.println(userPosts.get(0).getTitle());
         model.addAttribute("users", userService.getAllUsers());
         return "users";
     }
