141ab998b5feb4c119c01b24f6b7116290440fff
HenriqueValente09 <valentedev09@gmail.com>
2023-10-03 10:53:55 -0300

    chore: seed data

--- src/main/java/com/valenteNews/newsApp/config/SeedData.java
+++ src/main/java/com/valenteNews/newsApp/config/SeedData.java
@@ -1,12 +1,15 @@
 package com.valenteNews.newsApp.config;
 
+import com.valenteNews.newsApp.model.Post;
 import com.valenteNews.newsApp.model.User;
 import com.valenteNews.newsApp.repository.UserRepository;
+import com.valenteNews.newsApp.service.PostService;
 import com.valenteNews.newsApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +17,9 @@ import java.util.Optional;
 public class SeedData implements CommandLineRunner {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PostService postService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -30,6 +36,16 @@ public class SeedData implements CommandLineRunner {
             userService.save(user2);
         }
 
-
+        List<Post> posts = postService.getAllPosts();
+        if (posts.size() == 0) {
+            Post post1 = new Post();
+            post1.setTitle("Top 5 Bonecas Monster High");
+            post1.setContent("Lorem Ipsum");
+            post1.setImageURL("https://upload.wikimedia.org/wikipedia/pt/e/e5/MonsterHigh_Characters.png");
+            Optional<User> user = userService.getUserByName("João");
+            post1.setCreatedAt(LocalDateTime.now());
+            post1.setUser(user.get());
+            postService.save(post1);
+        }
     }
 }
