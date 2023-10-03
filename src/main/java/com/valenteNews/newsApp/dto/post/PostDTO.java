31e8d9886f221c3ab22206ec0865a88e5f08d64d
HenriqueValente09 <valentedev09@gmail.com>
2023-10-03 10:54:16 -0300

    chore: post dto more attributes

--- src/main/java/com/valenteNews/newsApp/dto/post/PostDTO.java
+++ src/main/java/com/valenteNews/newsApp/dto/post/PostDTO.java
@@ -1,9 +1,18 @@
 package com.valenteNews.newsApp.dto.post;
 
+import com.valenteNews.newsApp.model.User;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 public class PostDTO {
+    private String id;
     private String title;
     private String content;
+    private String imageURL;
+    private LocalDateTime createdAt;
+    private User user;
+
 }
