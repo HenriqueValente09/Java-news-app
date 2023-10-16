71a1b65cca94c9f5177d4a4f88e60018ce8464eb
HenriqueValente09 <valentedev09@gmail.com>
2023-10-16 16:48:55 -0300

    feat: Login route

--- src/main/java/com/valenteNews/newsApp/controller/UserController.java
+++ src/main/java/com/valenteNews/newsApp/controller/UserController.java
@@ -1,12 +1,21 @@
 package com.valenteNews.newsApp.controller;
 
+import com.valenteNews.newsApp.dto.auth.LoginResponseDTO;
 import com.valenteNews.newsApp.dto.post.RegisterPostDTO;
+import com.valenteNews.newsApp.dto.user.AuthDTO;
 import com.valenteNews.newsApp.dto.user.RegisterUserDTO;
 import com.valenteNews.newsApp.dto.user.UserDTO;
 import com.valenteNews.newsApp.mapper.UserMapper;
 import com.valenteNews.newsApp.model.User;
+import com.valenteNews.newsApp.service.TokenService;
 import com.valenteNews.newsApp.service.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +23,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -21,9 +31,12 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
+    private final TokenService tokenService;
 
     @GetMapping("/users")
-    public String users(Model model) {
+    public String users(Model model, HttpServletRequest request, Principal principal, HttpSession session) {
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        System.out.println(auth);
         List<UserDTO> userList = userMapper.UsersToUserDTO(userService.getAllUsers());
         model.addAttribute("users", userList);
         return "users";
@@ -34,5 +47,12 @@ public class UserController {
         RegisterUserDTO user = new RegisterUserDTO();
         model.addAttribute("user", user);
         return "registerUser";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        AuthDTO user = new AuthDTO();
+        model.addAttribute("user", user);
+        return "loginUser";
     }
 }
