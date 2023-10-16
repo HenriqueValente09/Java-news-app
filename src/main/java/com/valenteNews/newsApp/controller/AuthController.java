7fef5f427730c227b8dfe9759686bd72b8523029
HenriqueValente09 <valentedev09@gmail.com>
2023-10-16 16:51:01 -0300

    feat: logout endpoint

--- src/main/java/com/valenteNews/newsApp/controller/AuthController.java
+++ src/main/java/com/valenteNews/newsApp/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.valenteNews.newsApp.controller;
 
-import com.valenteNews.newsApp.dto.auth.LoginResponseDTO;
 import com.valenteNews.newsApp.dto.user.AuthDTO;
 import com.valenteNews.newsApp.dto.user.RegisterUserDTO;
 import com.valenteNews.newsApp.mapper.UserMapper;
@@ -9,13 +8,20 @@ import com.valenteNews.newsApp.model.UserRole;
 import com.valenteNews.newsApp.repository.UserRepository;
 import com.valenteNews.newsApp.service.TokenService;
 import com.valenteNews.newsApp.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("auth")
@@ -34,20 +40,31 @@ public class AuthController {
 
     @Autowired
     private UserMapper userMapper;
+
+    SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+
     @PostMapping("/login")
-    public ResponseEntity login(@ModelAttribute AuthDTO data){
-        System.out.println(data);
+    public String login(@ModelAttribute AuthDTO data, HttpServletResponse response, Principal principal, HttpSession session){
         var emailPassword = new UsernamePasswordAuthenticationToken(data.getEmail(), data.getPassword());
         var auth = this.authenticationManager.authenticate(emailPassword);
 
-        var token = tokenService.generateToken((User) auth.getPrincipal());
+        User user = (User) auth.getPrincipal();
+        session.setAttribute("user", user.getEmail());
+        session.setAttribute("user_role", user.getRole());
+        System.out.println(user.getName());
+        System.out.println(session.getId());
+        return "redirect:/users";
+    }
 
-        return ResponseEntity.ok(new LoginResponseDTO(token));
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        System.out.println(authentication);
+        this.logoutHandler.logout(request, response, authentication);
+        return "redirect:/home";
     }
 
     @PostMapping("/register")
     public String register(@ModelAttribute RegisterUserDTO data){
-        System.out.println(data);
         if(this.repository.findByEmail(data.getEmail()) != null) return "404";
 
         String encryptedPassword = new BCryptPasswordEncoder().encode(data.getPassword());
