5f22df7f41e83fb2c6c38b5fd034b70fefd0e54b
HenriqueValente09 <valentedev09@gmail.com>
2023-10-20 20:40:52 -0300

    feat: added pagination

--- src/main/java/com/valenteNews/newsApp/controller/UserController.java
+++ src/main/java/com/valenteNews/newsApp/controller/UserController.java
@@ -14,6 +14,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,9 +24,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @RequiredArgsConstructor
@@ -34,10 +40,22 @@ public class UserController {
     private final TokenService tokenService;
 
     @GetMapping("/users")
-    public String users(Model model, HttpServletRequest request, Principal principal, HttpSession session) {
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        System.out.println(auth);
+    public String users(Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size) {
         List<UserDTO> userList = userMapper.UsersToUserDTO(userService.getAllUsers());
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(2);
+
+        Page<User> userPage = userService.findPaginated(PageRequest.of(currentPage - 1, pageSize));
+        model.addAttribute("userPage", userPage);
+        int totalPages = userPage.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+            System.out.println(pageNumbers);
+        }
+
         model.addAttribute("users", userList);
         return "users";
     }
