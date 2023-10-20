d9e431a83b72b5dedc08bb8d3fca4c641fd1c695
HenriqueValente09 <valentedev09@gmail.com>
2023-10-20 20:40:21 -0300

    feat: added pagination

--- src/main/java/com/valenteNews/newsApp/service/UserService.java
+++ src/main/java/com/valenteNews/newsApp/service/UserService.java
@@ -3,9 +3,15 @@ package com.valenteNews.newsApp.service;
 import com.valenteNews.newsApp.model.User;
 import com.valenteNews.newsApp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +20,28 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+
+
+    public Page<User> findPaginated(Pageable pageable) {
+        List<User> users = userRepository.findAll();
+        int pageSize = pageable.getPageSize();
+        int currentPage = pageable.getPageNumber();
+        int startItem = currentPage * pageSize;
+        List<User> list;
+
+        if (users.size() < startItem) {
+            list = Collections.emptyList();
+        } else {
+            int toIndex = Math.min(startItem + pageSize, users.size());
+            list = users.subList(startItem, toIndex);
+        }
+
+        Page<User> userPage
+                = new PageImpl<User>(list, PageRequest.of(currentPage, pageSize), users.size());
+
+        return userPage;
+    }
 
     public User save(User user) {
         try{
